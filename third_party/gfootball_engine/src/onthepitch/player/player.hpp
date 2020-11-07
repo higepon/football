9d35c378f1a8ccdca5d17f42f1b634fda95de91a
Taro Minowa <higepon@gmail.com>
2020-11-07 11:17:20 +0900

    debug print

--- third_party/gfootball_engine/src/onthepitch/player/player.hpp
+++ third_party/gfootball_engine/src/onthepitch/player/player.hpp
@@ -122,7 +122,10 @@ class Player : public PlayerBase {
     virtual void ResetSituation(const Vector3 &focusPos);
 
     void ProcessState(EnvState* state);
-    float GetFatigueFactorInv() const { return GetTeamID() == 1 ? 1.0 : fatigueFactorInv; }
+    float GetFatigueFactorInv() const {
+      printf("GetFatigueFactorInv called %d", GetTeamID());
+      return GetTeamID() == 1 ? 1.0 : fatigueFactorInv;
+    }
   protected:
     void _CalculateTacticalSituation();
 
